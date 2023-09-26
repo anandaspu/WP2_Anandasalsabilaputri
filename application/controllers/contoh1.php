f49df04fc5b7ad81240e7eca39a80ce7ca68724d
anandaspu <anandasp30@gmail.com>
2023-09-26 20:58:37 +0700

    menambahkan contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Ananda Salsabila Putri
+ Saya tingga di daerah Bekasi
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
