635536a5b70e4336876bcd3fc9dc71f4944b8a02
anandaspu <anandasp30@gmail.com>
2023-10-06 12:36:52 +0700

    menambahkan .htaccess,config.php,Latihan1.php,Welcome.php,Model_Latihan1.php,Matakuliah.php,view-form-matakuliah.php,view-latihan.php

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,8 +1,18 @@
 <?php
-class Latihan1 extends CI_Controller
+class latihan1 extends CI_Controller
 {
- public function index()
- {
- echo "Selamat Datang.. selamat belajar Web Programming";
- }
+    public function index()
+    {
+        echo"Selamat Datang..";
+    }
+
+    public function penjumlahan ($nilai1, $nilai2)
+    {
+        $this->load->model('Model_latihan1');
+
+        $data['nilai1']= $nilai1;
+        $data['nilai2']= $nilai2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($nilai1, $nilai2);
+        $this->load->view('view-latihan', $data);
+    }
 }
